b8e99a2310c5aab4cdcdba1398f24c475734bb3c
Tenchi <tenchi@team2xh.net>
2015-12-29 11:45:56 +0100

    fix label positioning and reduce amount of magic numbers

--- falcon/puncher.py
+++ falcon/puncher.py
@@ -9,14 +9,22 @@ from .util import *
 font = ImageFont.truetype("resources/OpenSans-CondLight.ttf", 64)
 font_small = ImageFont.truetype("resources/OpenSans-CondLight.ttf", 36)
 
+margin_left = 75
+margin_right = 75
+card_width = 1024
+beat_height = 100
+
 
 def draw_labels(draw, box, y0=0):
     """
     :type param box: MusicBox
     """
+    x0 = margin_left - (font.getsize(box.labels[0][0])[0] / 2)
+    w = (card_width - margin_left - margin_right) / (len(box.labels) - 1.0)
+    print x0
     for i, label in enumerate(box.labels):
         elements = label if len(label) == 3 else [label[0], "", label[1]] if len(label) == 2 else [label[0], "", ""]
-        x = 60 + i * 30
+        x = x0 + i * w
         y = 40 if (i % 2 == 1) else 100
         draw.text((x, y + y0), elements[0], "black", font)
         letter_width, _ = font.getsize(elements[0])
@@ -37,27 +45,31 @@ def draw_arrow(draw, x0=0, y0=0, w=100, h=400):
 
     draw.polygon(points, fill="black")
 
-beat_height = 100
-
 
 def draw_grid(draw, beats, box, y0=0):
     h = beat_height
     l = len(box.notes)
-    w = (1024 - 150) / float(l - 1)
+    w = (card_width - margin_left - margin_right) / float(l - 1)
+    # Draw vertical lines, first and last thicker
     for i in xrange(l):
         t = 3 if (i == 0 or i == l - 1) else 1
-        draw.line([(75 + i * w, y0), (75 + i * w, y0 + beats * h)], fill="black", width=t)
+        draw.line([(margin_left + i * w, y0),
+                   (margin_left + i * w, y0 + beats * h)], fill="black", width=t)
     y = 0
-
+    # Draw half-beat horizontal lines
     for i in xrange(beats):
         y = y0 + h * i
-        draw.line([(75, y + h / 2), (1024 - 75, y + h / 2)], fill="black", width=1)
+        draw.line([(margin_left, y + h / 2),
+                   (card_width - margin_left, y + h / 2)], fill="black", width=1)
+    # Make half-beat lines dashed by drawing thick vertical white lines over
     for i in xrange(l - 1):
-        draw.line([(75 + i * w + w / 2, y0), (75 + i * w + w / 2, y0 + beats * h)], fill="white", width=10)
-
+        draw.line([(margin_left + i * w + w / 2, y0),
+                   (margin_left + i * w + w / 2, y0 + beats * h)], fill="white", width=10)
+    # Draw full-beat lines
     for i in xrange(beats + 1):
         y = y0 + h * i
-        draw.line([(75, y), (1024 - 75, y)], fill="black", width=3)
+        draw.line([(margin_left, y),
+                   (card_width - margin_left, y)], fill="black", width=3)
 
     return y
 
@@ -67,7 +79,7 @@ def draw_track(draw, track, resolution, box, y0=0):
     offsets = {pitch: i for i, pitch in enumerate(box.notes)}
 
     h = beat_height
-    w = (1024 - 150) / float(len(box.notes) - 1)
+    w = (card_width - margin_left - margin_right) / float(len(box.notes) - 1)
     r = 10
     t = 0
 
@@ -76,14 +88,14 @@ def draw_track(draw, track, resolution, box, y0=0):
         if event.is_event(midi.events.NoteOnEvent.statusmsg):
             y = h * (t / resolution)
             x = offsets[event.pitch] * w
-            draw.ellipse([(75 - r + x, y0 + y - r), (75 + r + x, y0 + y + r)], fill="black")
+            draw.ellipse([(margin_left - r + x, y0 + y - r), (margin_left + r + x, y0 + y + r)], fill="black")
 
 
 def punch(filename, tracks, box):
     """
     :type param box: MusicBox
     """
-    im = Image.new("RGB", (1024, 800 + beats(tracks) * beat_height), "white")
+    im = Image.new("RGB", (card_width, 800 + beats(tracks) * beat_height), "white")
     draw = ImageDraw.Draw(im)
 
     draw_labels(draw, box, y0=400)
